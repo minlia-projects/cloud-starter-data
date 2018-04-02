7c5682e31d2dffaf5fdbb17735bdc62d3f037006
Henry Lin <badcop@163.com>
2018-04-02 17:14:05 +0800

    up

---
+++ src/main/java/com/minlia/module/data/entity/AbstractEntity.java
@@ -0,0 +1,67 @@
+package com.minlia.module.data.entity;
+
+//import org.springframework.data.annotation.CreatedBy;
+//import org.springframework.data.annotation.CreatedDate;
+//import org.springframework.data.annotation.LastModifiedBy;
+//import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+//@JsonInclude(JsonInclude.Include.NON_NULL)
+//@JsonAutoDetect(
+//        fieldVisibility = JsonAutoDetect.Visibility.DEFAULT,
+//        getterVisibility = JsonAutoDetect.Visibility.NONE,
+//        setterVisibility = JsonAutoDetect.Visibility.NONE,
+//        isGetterVisibility = JsonAutoDetect.Visibility.NONE,
+//        creatorVisibility = JsonAutoDetect.Visibility.NONE)
+public abstract class AbstractEntity extends WithIdEntity {
+
+    private static final long serialVersionUID = 1L;
+
+//    @CreatedBy
+    @NotNull
+    private String createdBy;
+
+//    @CreatedDate
+    @NotNull
+    private Date createdDate = new Date();
+
+//    @LastModifiedBy
+    private String lastModifiedBy;
+
+//    @LastModifiedDate
+    private Date lastModifiedDate = new Date();
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreateDate() {
+        return createdDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createdDate = createDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
